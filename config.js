8ea1786eac7f9d5b711252426765fa8f5df7f200
Sadee-123 <nisalmuhandiram02@gmail.com>
2025-08-01 16:38:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined? "ENTER YOUR SESSION ID":process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined? "87FnVS7L#eOxKFLGMrAI8c_4YE2o0UfzcL3Gqe6virEzAO-Uk9fw":process.env.SESSION_ID,
 MONGODB: process.env.MONGODB || "ENTER YOUR MONGO DB URL",
 GEMINI_API_KEY: process.env.GEMINI_API_KEY || "AIzaSyDzyQ7tnQkquWX8bc1y9TE0auCHbC_QQO8",
 tokenm: process.env.GITHUB_TOKEN || "false",
